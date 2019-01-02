a787a09ab4a6e8d8b819f64b83eeac0427a2d814
whatisgalen <whatisgalen@gmail.com>
2019-01-01 23:41:35 -0800

    added final functionality for mvp

--- main.js
+++ main.js
@@ -19,34 +19,65 @@ async function main() {
         }; 
     }
 
-    let collected, censusTractsLayers;
+    //experimental
+    for(let i = 0; i < polyData.features.length;i++) {
+        polyData.features[i]["id"] = polyData.features[i].properties.name;
+    }
+
+    let collected, hasDensity, noDensity, censusTractsLayers;
     try {
         collected = turf.collect(polyData, pointData, "id", "marker_ids"); //create/populate prop in each poly called marker_ids
         for(let i = 0; i < collected.features.length; i++) {
             collected.features[i].properties["density"] = collected.features[i].properties.marker_ids.length;
+            
         }
-        censusTractsLayers = buildLayers(collected);
-        buildMap(censusTractsLayers);
+        hasDensity = { "type": "FeatureCollection", "features": [] };
+        noDensity = { "type": "FeatureCollection", "features": [] };
+        for(let i = 0; i < collected.features.length; i++) {
+            if(collected.features[i].properties.density == 0) {
+                noDensity.features.push(collected.features[i]);
+            } else {
+                hasDensity.features.push(collected.features[i]);
+            }
+        }
+        
+        // console.log("this is collected:");
+        console.log(collected);
+   
+        buildMap(collected, hasDensity, noDensity);
     } catch(e) {
         console.log(e);
     }
 }
 
 async function fetchData(url) {
-    const t0 = performance.now();
+    // const t0 = performance.now();
     const response = await fetch(url);
     const data = await response.json();
     // console.log("we got: "+data.json());
-    const t1 = performance.now();
-    console.log("fetchData="+((t1-t0)/1000));
-    return (data); //was: json.data();
+    // const t1 = performance.now();
+    // console.log("fetchData="+((t1-t0)/1000));
+    return (data);
 }
 
 function buildLayers(collected) {
-    var t0 = performance.now();
+    // var t0 = performance.now();
     let censusTractsLayers = [];
 
         censusTractsLayers.push(
+            {
+                id: "tracts-outline",
+                type: "line",
+                source: {
+                    type: 'geojson',
+                    data: collected
+                },
+                paint: {
+                    "line-color": ["case", ["boolean", ["feature-state", "hover"], !1], "#383838", "rgb(193, 193, 193)"],
+                    "line-width": ["case", ["boolean", ["feature-state", "hover"], !1], 3, 1]
+                }
+
+            },
             {
                 id: ('tractPoly1'),
                 type: 'fill-extrusion',
@@ -54,110 +85,212 @@ function buildLayers(collected) {
                     type: 'geojson',
                     data: collected
                 },
-                paint: {
+                paint: 
+                {
                     'fill-extrusion-color': [
                         'interpolate',
                         ['linear'],
                         ['get', 'density'],
-                        0, '#ffffcc',
-                        5, '#ffcc99',
-                        10, '#ff5500',
-                        20, '#cc0066',
-                        30, '#0000ff',
-                        40, '#00ccff',
-                        50, '#00e600'
+                        0, '#f5f5f5',
+                        50, '#d1ffff',
+                        100, '#a1ffff',
+                        150, '#71ffff',
+                        200, '#41ffff',
+                        250, '#11ffff',
+                        300, '#00efff',
+                        350, '#00d6ff',
+                        400, '#00bdff',
+                        450, '#00a4ff',
+                        500, '#0073ef',
+                        550, '#005cbf',
+                        600, '#00458f',
+                        650, '#002e5f',
+                        700, '#00172f'
                     ],
-                    'fill-extrusion-opacity': 0.5,
+                    'fill-extrusion-opacity': 0.65,
                     'fill-extrusion-height': [
-                        'interpolate',
-                        ['linear'],
-                        ['get', 'density'],
-                        
-                        0, 0,
-                        1, 100,
-                        2, 200,
-                        3, 300,
-                        4, 400,
-                        5, 500,
-                        6, 600,
-                        7, 700,
-                        8, 800,
-                        9, 900,
-                        10, 1000,
-                        11, 1100,
-                        12, 1200,
-                        13, 1300,
-                        14, 1400,
-                        15, 1500,
-                        16, 1600,
-                        17, 1700,
-                        18, 1800,
-                        19, 1900,
-                        20, 2000,
-                        21, 2100,
-                        22, 2200,
-                        23, 2300,
-                        24, 2400,
-                        25, 2500,
-                        26, 2600,
-                        27, 2700,
-                        28, 2800,
-                        29, 2900,
-                        30, 3000,
-                        31, 3100,
-                        32, 3200,
-                        33, 3300,
-                        34, 3400,
-                        35, 3500,
-                        36, 3600,
-                        37, 3700,
-                        38, 3800,
-                        39, 3900,
-                        40, 4000,
-                        41, 4100,
-                        42, 4200,
-                        43, 4300,
-                        44, 4400,
-                        45, 4500,
-                        46, 4600,
-                        47, 4700,
-                        48, 4800,
-                        49, 4900,
-                        50, 5000
+                        "+", ["*", ["get", "density"], 15], ["case", ["boolean", ["feature-state", "hover"], !1], 1e3, 0]
                     ],
+                    "fill-extrusion-base": ["case", ["boolean", ["feature-state", "hover"], !1], 1e3, 0],
+        
+                     
                 }
             }
         )
-    var t1 = performance.now();
-    console.log("BuildLayers="+((t1-t0)/1000)); 
+    // var t1 = performance.now();
+    // console.log("BuildLayers="+((t1-t0)/1000)); 
     return censusTractsLayers;
 }
 
-function buildMap(censusTractsLayers) {
-    var t0 = performance.now();
+function buildMap(collected, hasDensity, noDensity) {
+    // var t0 = performance.now();
+    let hoveredStateId = null;
     map.on('load', ()=> {
-        for(let m = 0; m < censusTractsLayers.length; m++) {
-            map.addLayer(censusTractsLayers[m]);
+
+        //build panels
+        const layers = ['1-49', '50-99', '100-149', '150-199', '200-249', '250-299', '300-349', '350-399','400-449','450-499','500-549','550-599','600-649','650-699','700+'];
+        const colors = ['#ffffff', '#d1ffff', '#a1ffff', '#71ffff', '#41ffff', '#11ffff', '#00efff', '#00d6ff','#00bdff','#00a4ff','#0073ef','#005cbf','#00458f','#002e5f','#00172f'];
+        for (let i = 0; i < layers.length; i++) {
+            let layer = layers[i];
+            let color = colors[i];
+            let item = document.createElement('div');
+            let key = document.createElement('span');
+            key.className = 'legend-key';
+            key.style.backgroundColor = color;
+          
+            let value = document.createElement('span');
+            value.innerHTML = layer;
+            item.appendChild(key);
+            item.appendChild(value);
+            legend.appendChild(item);
+          }
+
+
+        //add source(s)
+        map.addSource('collected-tracts', {
+            type: 'geojson',
+            data: collected
+        });
+        map.addSource('dense-tracts', {
+            type: 'geojson',
+            data: hasDensity
+        });
+        map.addSource('flat-tracts', {
+            type: 'geojson',
+            data: noDensity
+        });
+
+        //add layers
+        map.addLayer(
+            {
+                id: "tracts-outline",
+                type: "line",
+                source: 'collected-tracts',
+                paint: {
+                    // "line-color": ["case", ["boolean", ["feature-state", "hover"], false], "#383838", "rgb(193, 193, 193)"],
+                    "line-color": "rgb(193, 193, 193)",
+                    "line-width": ["case", ["boolean", ["feature-state", "hover"], false], 5, 1]
+                }
+            });
+        map.addLayer(
+            {
+                id: "tracts-extruded",
+                type: 'fill-extrusion',
+                source: 'dense-tracts',
+                paint: 
+                {
+                    "fill-extrusion-height": [
+                        "+", ["*", ["get", "density"], 15], ["case", ["boolean", ["feature-state", "hover"], false], 1e3, 0]
+                    ],
+                    "fill-extrusion-base": ["case", ["boolean", ["feature-state", "hover"], false], 1e3, 0],
+                    "fill-extrusion-color": {
+                        property: "density",
+                        stops: 
+                        [[1, '#ffffff'],
+                        [50, '#d1ffff'],
+                        [100, '#a1ffff'],
+                        [150, '#71ffff'],
+                        [200, '#41ffff'],
+                        [250, '#11ffff'],
+                        [300, '#00efff'],
+                        [350, '#00d6ff'],
+                        [400, '#00bdff'],
+                        [450, '#00a4ff'],
+                        [500, '#0073ef'],
+                        [550, '#005cbf'],
+                        [600, '#00458f'],
+                        [650, '#002e5f'],
+                        [700, '#00172f']]
+                    },
+                    "fill-extrusion-opacity": 0.725
+                      
+                }
+            });
+            map.addLayer(
+                {
+                    id: "tracts-non-extruded",
+                    type: 'fill-extrusion',
+                    source: 'flat-tracts',
+                    paint: 
+                    {
+                        "fill-extrusion-height": 0,
+                        "fill-extrusion-base": 0,
+                        // "fill-extrusion-color": {},
+                        "fill-extrusion-opacity": 0
+                          
+                    }
+                });
+    // });
+    // var t1 = performance.now();
+    // console.log("BuildMap="+((t1-t0)/1000));
+
+    //let popup = new mapboxgl.Popup();
+
+    
+    map.on("mousemove", "tracts-extruded", (e)=> {
+        
+        if(e.features.length > 0) {
+            if(hoveredStateId) { 
+                map.setFeatureState({source: 'dense-tracts', id: hoveredStateId}, { hover: false});
+                map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: false});
+            }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState({source: 'dense-tracts', id: hoveredStateId}, {hover: true});
+            map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: true});
+            // console.log(e.features[0].id);
+        }
+        
+    });
+    map.on("mousemove", "tracts-non-extruded", (e)=> {
+        
+        if(e.features.length > 0) {
+            if(hoveredStateId) { 
+                map.setFeatureState({source: 'dense-tracts', id: hoveredStateId}, {hover: false});
+                map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, { hover: false});
+            }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: true});
+            // console.log(e.features[0].id);
+        
         }
     });
-    var t1 = performance.now();
-    console.log("BuildMap="+((t1-t0)/1000));
 
-//   let popup = new mapboxgl.Popup();
+    map.on("mouseleave", "tracts-extruded", ()=> {
+        if(hoveredStateId) {
+            map.setFeatureState({source: 'dense-tracts', id: hoveredStateId}, {hover: false});
+            map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: false});
+        }
+        hoveredStateId = null;
+    });
 
-map.on('mousemove', function(e) {
-  let features = map.queryRenderedFeatures(e.point ) //
-  if (!features.length) {
-//     popup.remove();
-    return;
-  }
+    map.on("mouseleave", "tracts-non-extruded", ()=> {
+        if(hoveredStateId) {
+            map.setFeatureState({source: 'dense-tracts', id: hoveredStateId}, {hover: false});
+            map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: false});
+        }
+        hoveredStateId = null;
+    });
 
+    map.on('mousemove', (e)=> {
+        let features = map.queryRenderedFeatures(e.point);
+    
+        if (features.length > 0) {
+            
+            document.getElementById('pd').innerHTML = '<h3><strong>ct#' + features[0].properties.name + '</strong></h3><p><strong><em>' + features[0].properties.density + '</strong> historical landmarks here!</em></p>';
+        } else {
+            document.getElementById('pd').innerHTML = '<p>Hover over a census tract!</p>';
+        }
+        map.getCanvas().style.cursor = features.length ? 'pointer' : ''; 
+    });
 
-  map.getCanvas().style.cursor = features.length ? 'pointer' : '';
-});
+    // map.on("mouseleave", "tracts-outline", ()=> {
+    //     if(hoveredStateId) {
+    //         map.setFeatureState({source: 'collected-tracts', id: hoveredStateId}, {hover: false});
+    //     }
+    //     hoveredStateId = null;
+    // });
 
-map.on('click', function(e) {
-  
-  });
+    // map.on('click', function(e) {});
+    });
 }
 
